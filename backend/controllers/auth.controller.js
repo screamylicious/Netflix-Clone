512c9c744f059b62ace2689d574dbeba3f89d64d
Abhinav Kumbhare <screamylicious@Mac-Ab.local>
2025-02-02 22:58:26 +0530

    backend added

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,59 @@
+import bcryptjs from "bcryptjs";
+import { User } from "../models/user.model.js";
+export async function signup(req, res) {
+  try {
+    const { email, password, username } = req.body;
+    if (!email || !password || !username)
+      return res
+        .status(400)
+        .json({ success: false, message: "All fields are required" });
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email))
+      return res.status(400).json({ success: false, message: "invalid email" });
+    if (password.length < 6)
+      return res.status(400).json({
+        success: false,
+        message: "Password must be atleast 6 characters",
+      });
+    const existingUserByEmail = await User.findOne({ email });
+    if (existingUserByEmail)
+      return res
+        .status(400)
+        .json({ success: false, message: "Email already exists" });
+
+    const existingUserByUsername = await User.findOne({ username: username });
+    if (existingUserByUsername)
+      return res
+        .status(400)
+        .json({ success: false, message: "Username already exists" });
+
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+
+    const PROFILE_PICS = ["/avatar1.png", "/avatar2.png", "/avatar3.png"];
+    const image = PROFILE_PICS[Math.floor(Math.random() * PROFILE_PICS.length)];
+
+    const newUser = new User({
+      email,
+      password: hashedPassword,
+      username,
+      image,
+    });
+    await newUser.save();
+
+    const { password: _, ...userData } = newUser._doc;
+
+    return res.status(201).json({ success: true, user: userData });
+  } catch (error) {
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+}
+
+export async function login(req, res) {
+  res.send("Login Route");
+}
+
+export async function logout(req, res) {
+  res.send("Logout Route");
+}
